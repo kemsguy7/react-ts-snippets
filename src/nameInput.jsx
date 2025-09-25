f461d70ea48be127583fec349406cfd5331e31e4
kemsguy7 <mattidungafa@gmail.com>
2025-09-25 19:23:57 +0100

    updates: lifting state up in nameInput component

--- src/nameInput.jsx
+++ src/nameInput.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 
-const NameInput = (props) => {
+const NameInput = ({ name, setName }) => {
   return (
     <div>
       <label>
         Enter your name :
-        <input type='text' value={props.name} onChange={(e) => props.setName(e.target.value)} />
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
       </label>
     </div>
   );
 };
 
-const Greeting = (props) => {
-  return <h1>Hello, {props.name}! </h1>;
+const Greeting = ({ name }) => {
+  return <h1>Hello, {name}! </h1>;
 };
 
 const NameApp = () => {
