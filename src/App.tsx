f6e85fcfc7b8a7d55b7ce1cb0b774e40773b70c9
kemsguy7 <mattidungafa@gmail.com>
2025-09-27 23:57:23 +0100

    updates: added Usercard component to app.tsx with props

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Counter from './Counter';
 import NameForm from './NameForm';
 import NameApp from './nameInput';
+import UserCard from './UserCard';
 import './App.css';
 
 interface GreetingProps {
@@ -12,10 +13,16 @@ const Greeting: React.FC<GreetingProps> = ({ name }) => {
   return <h1> Hello, {name}! </h1>;
 };
 
+const handleUserClick = (id: number) => {
+  alert(`User with ID ${id} clicked!`);
+};
+const user = { id: 4, name: 'Kemsguy7', email: 'mattidungafa@gmail.com' };
+
 const App: React.FC = () => {
   return (
     <div>
       <Greeting name='React Developer' />
+      <UserCard user={user} onClick={handleUserClick} />
       <Counter />
       <NameForm />
       <NameApp />
