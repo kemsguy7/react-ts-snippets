3fdb87b4b1a3e5793d09dce6825acb5162814f55
kemsguy7 <mattidungafa@gmail.com>
2025-09-30 18:31:36 +0100

    updaete: started with with sythentic event handlers

---
+++ src/components/chap2/synth.tsx
@@ -0,0 +1,28 @@
+{
+  /* Typing Event Handlers */
+}
+
+import React from 'react';
+import { useState } from 'react';
+
+const ClickButton: React.FC = () => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('Button Clicked');
+  };
+  return <button onClick={handleClick}> Click Me </button>;
+};
+
+//typing input field event handler
+export const InputField: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  {
+    /* React.changeEvent<HTMLInputElement> ensures that the event is correctly typed for an input field */
+  }
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  return <input type='text' value={value} onChange={handleChange} />;
+};
+
+export default ClickButton;
