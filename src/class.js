b0b6aed85c48915f79b16ac20b13845275838f75
kemsguy7 <mattidungafa@gmail.com>
2025-10-02 13:18:07 +0100

    updates: added getter and setter functions

--- src/class.js
+++ src/class.js
@@ -6,8 +6,19 @@ class Person {
   getName() {
     return this.name;
   }
+
+  setName(newName) {
+    newName = newName.trim();
+    if (newName === '') {
+      throw 'Then name cannot be empty';
+    }
+    this.name = newName;
+  }
 }
 
-let Matthew = new Person('Matthew');
-console.log(Matthew.getName());
-console.log(typeof Person);
+let person = new Person('Matthew');
+console.log(person.getName());
+
+console.log('Setting New Name');
+person.setName(' Matthew Idungafa ');
+console.log(person.getName());
